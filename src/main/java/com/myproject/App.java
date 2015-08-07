15a1423471d7401a2445fda46451b0c03f96fde3
shajeers <shajeers@gmail.com>
2015-08-07 15:31:03 +0530

    from eclipse2

--- src/main/java/com/myproject/App.java
+++ src/main/java/com/myproject/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worlds modified from eclipse!" );
+        System.out.println( "Hello Worlds modified from eclipse2!" );
     }
 }
